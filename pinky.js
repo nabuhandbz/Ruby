28ecf0bcb98e7032b56037a5519803e12eb6d8c0
nabuhan-dbz <93925348+nabuhandbz@users.noreply.github.com>
2022-04-19 10:35:53 +0530

    Update pinky.js

--- pinky.js
+++ pinky.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+918301985859' : process.env.NUMBER,   
+ PHONE: process.env.NUMBER === undefined ? '+918289836232 : process.env.NUMBER,   
  OA_NAME: process.env.DEPLOYER === undefined ? 'Someone' : process.env.DEPLOYER, 
  CODDED_BY: "afnanplk",    
 };
